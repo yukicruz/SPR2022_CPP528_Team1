2fcd99d5020da8b34869e7050bfedfa38a6e9754
krbrick <66096013+krbrick@users.noreply.github.com>
2022-03-29 14:53:42 -0400

    trying to get first two functions to work
    no luck yet

--- Utilities.R
+++ Utilities.R
@@ -7,8 +7,7 @@ DD_FILEPATH <- here::here( "data/rodeo/ltdb_data_dictionary.csv" )
 # import data dictionary
 dd <- read.csv( DD_FILEPATH, stringsAsFactors=F )
 
-# create a function that searches variable descriptions for a specific string and returns any 
-# that match. 
+# ONE: Filter variables by theme or group. Write a function that takes as an argument one or more of the groups that you just created in the concordance file under the “category” field and returns all of the variables associated with that group.
 aapi <- c (str_detect(dd$definition, regex("Asian", ignore_case = T)),
            str_detect(dd$definition, regex("pacific", ignore_case = T)),
            str_detect(dd$definition, regex("korean", ignore_case = T)),
@@ -20,39 +19,63 @@ asians.pacific.islanders <- cbind(dd,aapi) %>%
 View(asians.pacific.islanders)
 
 
+# another example 
+
 dd %>% filter(str_detect(definition, "poverty")) %>%
   slice()
+
+
+# The function should return all of the rows of the dataframe that belong to the group.
+
+
+
+# TWO: create a function that searches variable descriptions for a specific string and returns any 
+# that match. 
+
+# does not work
+
+find.variables.for.string <- function(variable.string) {
+  test.term <- str_detect(dd$definition, regex("variable.string", ignore_case = T))
+    test.term.l <- cbind (dd,test.term) %>%
+      filter(test.term == T)
+  return(test.term.l)
+}
+
+find.variables.for.string(asian)
+
+#function back work - does not work
+
+asian.test <- str_detect(dd$definition, regex("Asian", ignore_case = T))
+                         asian.test.2 <-cbind(dd,asian.test) %>%
+                           filter(asian.test == T)
+                         View(asian.test.2)
+
+find.variables.for.string(asian)
+
+#does not work
+
+find.string <- function(string) {
+  variable.string <- dd %>% filter(str_detect(definition, "string")) 
+  return(variable.string)
+}
+
+find.string(poverty)
+
+
+
+
 
 # Create a function to filter variables by time periods. 
 #Specifically, the user will specify the time periods of interest for the study and the
 #function will identify all variables that have measures for those periods.
 
-# choose from time periods:
-variables.1970s
-variables.1980s
-variables.1990s
-variables.2000s
-variables.2010s
-
-
-find.variables.for.time.period <- function(time.period) {
-  variables <- time.period %>% 
-    select(4)
-  return(variables)
-}
-
-
-
-find.variables.for.time.period("your time period here")
-
-#need to remove NAs and blanks from function
-
+#variable formation:
 seventies <- c (grepl("[1-9]",dd$X1970.f),
                 grepl("[1-9]",dd$X1970.s))
 
 variables.1970s <- cbind (dd,seventies) %>%
   filter(seventies == T)
-                
+
 
 
 eighties <- c (grepl("[1-9]",dd$X1980.f),
@@ -87,6 +110,27 @@ later.2000s<- c(grepl("[1-9]",dd$X2010.f),
 
 variables.2010s <- cbind (dd,later.2000s) %>%
   filter(later.2000s == T)
+# choose from time periods:
+variables.1970s
+variables.1980s
+variables.1990s
+variables.2000s
+variables.2010s
+
+# function
+find.variables.for.time.period <- function(time.period) {
+  variables <- time.period %>% 
+    select(4)
+  return(variables)
+}
+
+
+find.variables.for.time.period("your time period here")
+
+
+#need to remove NAs and blanks from function
+
+
 
 #BONUS: create a function that adds a column to the current LTDB dataset
 # does not work don't fool yourselves. just playing around at 3am

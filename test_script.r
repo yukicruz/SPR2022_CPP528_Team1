3b34993a0d7e8ede4349b8657838d96e2c888756
jmacost5 <54372707+jmacost5@users.noreply.github.com>
2022-04-06 10:04:36 -0700

    Create test_script.r

---
+++ test_script.r
@@ -0,0 +1,8 @@
+#Author : Jestrii Acosta
+#Date : April 06 2022
+#Purpose: Test renv's static analysis to document package dependencies
+
+
+# load necessary packages ----
+library(here)
+library(tidyverse)
